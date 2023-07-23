7355d1359a71193cd184662ba174fe0d4c214be5
Abhinav Rawat <77267335+abhinavR03@users.noreply.github.com>
2023-07-23 23:07:19 +0530

    Update Reverse a linked list recursively.cpp

--- C++/Linked List/Reverse a linked list recursively.cpp
+++ C++/Linked List/Reverse a linked list recursively.cpp
@@ -1,3 +1,50 @@
-void reverse_list(list_node *head){
-    list_node *
+#include<iostream>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+ListNode* reverseList(ListNode* head) {
+    if (head == nullptr || head->next == nullptr) {
+        return head;
+    }
+    ListNode* newHead = reverseList(head->next);
+    head->next->next = head;
+    head->next = nullptr;
+
+    return newHead;
+}
+
+void push(ListNode*& head, int value) {
+    ListNode* newNode = new ListNode(value);
+    newNode->next = head;
+    head = newNode;
+}
+
+void printL(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout <<endl;
+}
+
+int main() {
+    ListNode* head = nullptr;
+    push(head, 1);
+    push(head, 2);
+    push(head, 3);
+    push(head, 4);
+    push(head, 5);
+
+    cout << "Original List: ";
+    printL(head);
+
+    ListNode* reversedHead = reverseList(head);
+    cout << "Reversed List: ";
+    printL(reversedHead); 
+
+    return 0;
 }
